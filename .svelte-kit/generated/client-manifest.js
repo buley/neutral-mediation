add1a93c87af68a88f79f559106b3c12772ddb8d
Taylor Buley <buley@outlook.com>
2022-06-28 21:16:12 -0700

    svelt

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -1,10 +1,19 @@
 export { matchers } from './client-matchers.js';
 
 export const components = [
-	() => import("../runtime/components/layout.svelte"),
-	() => import("../runtime/components/error.svelte")
+	() => import("../../src/routes/__layout.svelte"),
+	() => import("../runtime/components/error.svelte"),
+	() => import("../../src/routes/about.svelte"),
+	() => import("../../src/routes/index.svelte"),
+	() => import("../../src/routes/mediation.svelte"),
+	() => import("../../src/routes/template.svelte"),
+	() => import("../../src/routes/todos/index.svelte")
 ];
 
 export const dictionary = {
-	
+	"": [[0, 3], [1]],
+	"about": [[0, 2], [1]],
+	"mediation": [[0, 4], [1]],
+	"template": [[0, 5], [1]],
+	"todos": [[0, 6], [1], 1]
 };
