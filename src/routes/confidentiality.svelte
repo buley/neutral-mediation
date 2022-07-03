afd64ecb94584c6430dd3be3bec4e2e004656b55
Taylor Buley <buley@outlook.com>
2022-07-03 15:12:49 -0700

    content

--- src/routes/confidentiality.svelte
+++ src/routes/confidentiality.svelte
@@ -13,20 +13,12 @@
 <div class="content">
 	<h1>Confidentiality</h1>
 
-	<p>To promote transparency in mediation, California law generally makes mediation a confidential process. California's mediation confidentiality laws are laid out in Evidence Code sections <a href="https://leginfo.legislature.ca.gov/faces/codes_displaySection.xhtml?sectionNum=703.5.&amp;lawCode=EVID">703.5</a> and 1115 to 1129. Those laws establish the confidentiality of mediation and limit the disclosure, admissibility, and a court's consideration of communications, writings, and conduct in connection with a mediation. Generally, those laws mean the following:</p>
+	<p>To promote transparency in mediation, California law generally makes mediation a confidential process. California's mediation confidentiality laws are laid out in Evidence Code sections <a href="https://leginfo.legislature.ca.gov/faces/codes_displaySection.xhtml?sectionNum=703.5.&amp;lawCode=EVID">703.5</a> and <a href="https://codes.findlaw.com/ca/evidence-code/evid-sect-1115.html">1115 to 1129</a>. Those laws establish the confidentiality of mediation and limit the disclosure, admissibility, and a court's consideration of communications, writings, and conduct in connection with a mediation. Generally, those laws mean the following:</p>
 
 	<ul><li>All communications, negotiations, or settlement offers in the course of a mediation must remain confidential.</li><li>Statements made and writings prepared in connection with a mediation are not admissible or subject to discovery or compelled disclosure in noncriminal proceedings.</li><li>A mediator's report, opinion, recommendation, or finding about what occurred in a mediation may not be submitted to or considered by a court or another adjudicative body. </li><li>A mediator cannot testify in any subsequent civil proceeding about any communication or conduct occurring at, or in connection with, a mediation.</li></ul>
-	<p>Mediations not legally binding by default.&nbsp; </p>
-	<p>Mediation acts in a dozen states.</p>
-	<p>Is a meditation admissible in court? Yes. Confidential but not private. Be careful not to oversell.&nbsp;</p>
-	<p>Possible interpretations of confidentiality:</p>
-	<ul><li>I can share with others whose support is necessary to implement an agreement.</li><li>I can share with my professional advisors.</li><li>Doesn’t leave the mediation room.</li><li>My only limitation is seeking to enter mediation disclosures as evidence in civil litigation, arbitration, or administrative procedures.</li><li>I can report any confessed illegal activity to law enforcement personnel.</li></ul>
-	<p>Qualifications to model code of confidentiality (12 states):</p>
-	<ul><li>Is it a criminal proceeding?</li><li>The threat of imminent harm (Tarasoff case)&nbsp;</li><li>Child/Eder abuse statutes<ul><li>Usually not obligated, but permitted</li><li>Mandatory/Permissible reporter</li></ul></li><li>Fruit of the Poisonous Tree Doctrine<ul><li>What a person says is not admissible</li></ul></li><li>Allegations of malpractice Attorneys or Mediators?</li></ul>
-	<p>Fuzzy confidentially</p>
-	<ul><li>Build trust by showing confidentiality</li></ul>
-	<p>California</p>
-	<ul><li>Must be written to be legally enforceable in some jurisdictions, including California<ul><li>If not written agreement, wasn’t an agreement</li></ul></li><li>The agreement has to state explicitly that the agreement is intended to be “legally binding” (or words to that effect).</li><li>Include a clause for a settlement recite all material terms</li><li>Include a clause for a settlement agreement to wave confidentiality for purposes of enforcement and interpretation.<ul><li>People against this say open can of worms.. People are looking for closure.</li></ul></li></ul>
+	
+	<h2>Binding Mediated Agreements in California</h2>
+	<p>Agreements during confidential sessions be written to be legally enforceable in some jurisdictions, including California. The agreement has to state explicitly that the agreement is intended to be “legally binding” (or words to that effect), include a clause for a settlement recite all material terms, and, crucially, nclude a clause for a settlement agreement to wave confidentiality for purposes of enforcement and interpretation.</p>
 
 </div>
 <style>
