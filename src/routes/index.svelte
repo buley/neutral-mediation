26be662023ea78c5fb470c4a47920348a34ca8ef
Taylor Buley <buley@outlook.com>
2022-07-02 17:12:29 -0700

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -87,7 +87,7 @@
 
 	.welcome-book-text {
 		margin: 0 auto 40px auto;
-		padding: 5px 10px 5px 10px;
+		padding: 10px 10px 5px 10px;
 		vertical-align: middle;
 	    font-family: var(--sans-serif-font);
 		background-image: linear-gradient(to right, var(--primary-color), var(--secondary-color), var(--tertiary-color));
@@ -112,5 +112,36 @@
 		}
 	}
 
+	button {
+	  background-color: #fff;
+	  border: 1px solid #d5d9d9;
+	  border-radius: 8px;
+	  box-shadow: rgba(213, 217, 217, .5) 0 2px 5px 0;
+	  box-sizing: border-box;
+	  color: #0f1111;
+	  cursor: pointer;
+	  display: inline-block;
+	  font-family: "Amazon Ember",sans-serif;
+	  font-size: 13px;
+	  line-height: 29px;
+	  padding: 20px 10px 0 11px;
+	  position: relative;
+	  text-align: center;
+	  text-decoration: none;
+	  user-select: none;
+	  -webkit-user-select: none;
+	  touch-action: manipulation;
+	  vertical-align: middle;
+	}
+
+	button:hover {
+	  background-color: #f7fafa;
+	}
+
+	button:focus {
+	  border-color: #008296;
+	  box-shadow: rgba(213, 217, 217, .5) 0 2px 5px 0;
+	  outline: 0;
+	}
 
 </style>
