337fb51e6c7cb0f541d94a159e6abd94adbe1d04
Taylor Buley <buley@outlook.com>
2022-07-02 18:40:22 -0700

    consulting

--- src/routes/consulting.svelte
+++ src/routes/consulting.svelte
@@ -18,27 +18,27 @@
 
 	<h3>Conflict Consulting</h3>
 
-	<p>Neutral led conflict training session for one, four or eight hours, typically on-site at a business. Business consulting to address organizational systems that generate chronic conflict, including investigation, training and follow-through.</p>
+	<p>Neutral-led conflict virtual training session for one, four or eight hours, typically on-site at a business. Business consulting to address organizational systems that generate chronic conflict, including investigation, training and follow-through.</p>
 
 	<h2>Mediation</h2>
 
 	<h3>Mediation Consulting</h3>
 
-	<p>Neutral led consulting session on the topic of mediation for one, four or eight hours. This consulting is typically an exploration of the values of mediation, in general or with Neutral Mediation specifically, for a given situation, as part of an exploration of options for a particular dispute.</p>
+	<p>Neutral-led virtual consulting session on the topic of mediation for one, four or eight hours. This consulting is typically an exploration of the values of mediation, in general or with Neutral Mediation specifically, for a given situation, as part of an exploration of options for a particular dispute.</p>
 
 	<h3>Mediation Training</h3>
 
-	<p>Neutral led mediation training session for one, four or eight hours. Training can be directed education or specifically-focused training around a specific negotiation.</p>
+	<p>Neutral-led mediation virtual training session for one, four or eight hours. Training can be course-guided education or specifically-focused training around a specific negotiation.</p>
 
 	<h2>Negotiation</h2>
 
 	<h3>Negotiation Consulting</h3>
 	
-	<p>Neutral led negotiation consulting session for one, four or eight hours. Consulting is typically in regard to a specific negotiation or similar set of negotiations.</p>
+	<p>Neutral-led negotiation virtual consulting session for one, four or eight hours. Consulting is typically in regard to a specific negotiation or similar set of negotiations.</p>
 
 	<h3>Negotiation Training</h3>
 
-	<p>Neutral led negotiation training session for one, four or eight hours. Training can be directed education or specifically-focused training around a specific negotiation.</p>
+	<p>Neutral-led negotiation virtual training session for one, four or eight hours. Training can be course-guided education or specifically-focused training around a specific negotiation.</p>
 
 </div>
 
