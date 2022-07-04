b7e8be33817479405f0860d1a448d4cffb1351ab
Taylor Buley <buley@outlook.com>
2022-07-04 15:29:18 -0700

    .

--- src/routes/availability.svelte
+++ src/routes/availability.svelte
@@ -15,21 +15,25 @@
 	<p>Here's a list of what services are available when. Neutral Mediation's <a href="https://neutral.setmore.com/">booking schedule</a> is based around netural availability and the necessary length of the session.</p>
 	<h3>Conflict</h3>
 	<ul>
-	    <li>Conflict Coaching – Wednesdays, Thursdays & Fridays</li>
+	    <li>Conflict Coaching – Monday through Friday</li>
+	</ul>
+	<h3>Conflict Coaching</h3>
+	<ul>
+	    <li>Conflict Coaching – Monday through Friday</li>
 	</ul>
 	<h3>Mediation</h3>
 	<ul>
-	    <li>Meditation Trainings – Saturdays & Sundays</li>
-	    <li>Mediation Consultations – Wednesdays, Thursdays & Fridays</li>
+	    <li>Mediation Consultations – Monday through Friday</li>
 	    <li>Half-Day Mediations – Saturdays & Sundays</li>
 	    <li>Full-Day Mediations – Saturdays & Sundays</li>
+	    <li>Meditation Trainings – Saturdays & Sundays</li>
     </ul>
 	<h3>Negotiation</h3>
 	<ul>
-	    <li>Negotiation Trainings – Saturdays & Sundays</li>
-    	<li>Negotiation Consultation – Wednesdays, Thursdays & Fridays</li>
+    	<li>Negotiation Consultations – Monday through Friday</li>
     	<li>Half-Day Negotiations – Saturdays & Sundays</li>
     	<li>Full-Day Negotiations – Saturdays & Sundays</li>
+	    <li>Negotiation Trainings – Saturdays & Sundays</li>
 	</ul>
 </div>
 
