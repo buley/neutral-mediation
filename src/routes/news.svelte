ac0283e94affb027ef5968f5b117e34dc0ec1918
Taylor Buley <buley@outlook.com>
2022-07-03 22:51:59 -0700

    .

--- src/routes/news.svelte
+++ src/routes/news.svelte
@@ -10,15 +10,19 @@
 			const apiKey = process.env.PINBOARD_API_KEY || "tb:09609A3FC3DCC28DDEAC";
 			const pinboard = new Pinboard(apiKey)
 			if (!!apiKey) {
+				console.log("Running Pinboard API request", apiKey)
 				pinboard.get({ tag: ['negotiation', 'mediation'] }, (err, res) => {
+					console.log("Finished Pinboard API request", res)
 					let linksStack = []
 					if (null === err) {
 						const posts = res.posts
 						const updated = new Date(res.date)
 						for (let x = 0; x < posts.length; x += 1) {
 							let d = posts[x]
+							console.log("ITEM",d);
 							if ('yes' == d.shared) {
 								linksStack.push(d)
+								htmlContent += d.href;
 							}
 						}
 						resolve(linksStack)
