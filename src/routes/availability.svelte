6d9301384dbd53f5f8d036f946e899c8077e89f8
Taylor Buley <buley@outlook.com>
2022-07-04 15:28:04 -0700

    .

--- src/routes/availability.svelte
+++ src/routes/availability.svelte
@@ -19,17 +19,17 @@
 	</ul>
 	<h3>Mediation</h3>
 	<ul>
-	    <li>Meditation Trainings – Saturdays</li>
+	    <li>Meditation Trainings – Saturdays & Sundays</li>
 	    <li>Mediation Consultations – Wednesdays, Thursdays & Fridays</li>
-	    <li>Half-Day Mediations – Sundays</li>
-	    <li>Full-Day Mediations – Sundays</li>
+	    <li>Half-Day Mediations – Saturdays & Sundays</li>
+	    <li>Full-Day Mediations – Saturdays & Sundays</li>
     </ul>
 	<h3>Negotiation</h3>
 	<ul>
-	    <li>Negotiation Trainings – Saturdays</li>
+	    <li>Negotiation Trainings – Saturdays & Sundays</li>
     	<li>Negotiation Consultation – Wednesdays, Thursdays & Fridays</li>
-    	<li>Half-Day Negotiations – Sundays</li>
-    	<li>Full-Day Negotiations – Sundays</li>
+    	<li>Half-Day Negotiations – Saturdays & Sundays</li>
+    	<li>Full-Day Negotiations – Saturdays & Sundays</li>
 	</ul>
 </div>
 
