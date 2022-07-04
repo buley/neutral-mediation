afb7586800af40e0e3eaf6ab0f1a9bc41bcd6b12
Taylor Buley <buley@outlook.com>
2022-07-04 00:47:59 -0700

    content

--- src/routes/mediation.svelte
+++ src/routes/mediation.svelte
@@ -25,7 +25,7 @@
 	<h2>Our Practice</h2>
 
 	<p>
-		Neutral Mediation offers flexibly-scheduled virtual mediation sessions led by a trained professional neutral. <em>Mediation sessions start at $400 per participant.</em> For mediation consulting, see our <a href="/consulting">consulting page</a>. For more information about lunch stipends for full-day sessions, see our <a href="/pricing">pricing page</a>.
+		Neutral Mediation offers flexibly-scheduled virtual mediation sessions led by a trained professional neutral. <em>Mediation sessions start at $400 per participant.</em> For mediation consulting, see our <a href="/consulting">consulting page</a>. For more information about lunch stipends for full-day sessions, see our <a href="/prices">pricing page</a>.
 	</p>
 
 	<h2>Our Value</h2>
