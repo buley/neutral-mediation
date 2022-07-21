8086a4ad25211bd46d8a2380301e36fb5f69a42d
Taylor Buley <buley@outlook.com>
2022-07-21 00:32:56 -0700

    .

---
+++ src/routes/forms/goals.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+	export const hydrate = dev;
+	export const router = browser;
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Information Security</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<div class="content">
+	<h1>Goal Discovery Form</h1>
+	<p>
+		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdeRm9vg250M0qh7HohEryMR3ChC1SFsM8HMYGCznNewrQ35w/viewform" border=0 style="width:100%;min-height:800px;">
+	</p>
+</div>
