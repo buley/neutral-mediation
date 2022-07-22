2f237ec87ce5ab69c617d257edcbb59fd277da25
Taylor Buley <buley@outlook.com>
2022-07-21 23:26:39 -0700

    .

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -24,6 +24,8 @@
 
 	<p>Taylor Buley founded Neutral Mediation to minimize the cognitive and financial expense, time and trauma of embroiling complex disputes. As a professional third-party neutral, Taylor creates conversational spaces of trust, openness and psychological safety where he offers mediation, negotiation, consulting, training and conflict coaching services to help clients find paths forward beyond conflict.</p>
 
+	<p><em>Neutral Mediation’s EIN is 87-2105551. <a href="/w9">Click here for a downloadable W-9</a>.</em></p>
+
 	<h2>Our Values</h2>
 
 	<h3>Mission Statement</h3>
@@ -34,15 +36,11 @@
 
 	<p>Neutral Mediation exists to create an informal and safe environment for people to hear and be heard while pursuing creative options and solutions to relationship problems. It views each conflict as an opportunity for practicing peace and seeks resolution beyond dispute with empathy, education and empowerment.</p>
 
-	<p>Neutral Mediation values, above all else, ethics and personal integrity. It believes that a mediator with integrity should be neutral in name, word and deed. It believes in trusting others, too, to do what they say act in accordance with their stated values.</p>
+	<p>Neutral Mediation values, above all else, ethics and personal integrity. It believes that a mediator with integrity should be neutral in name, word and deed. It believes that a neutral’s words matter and should be used only necessarily, and that a neutral’s word matters and should be indefatigably trustworthy. It believes in trusting others, too, to do what they say act in accordance with their stated values.</p>
 
 	<p>Neutral Mediation believes in curiousity and acceptance. It believes in validating experience and feelings. It believes that with diversity comes the blessing of multiple viewpoints and an opportunity for commonality. Neutral Mediation is relentlessly welcoming of diverse opinions and backgrounds. It believes in face-to-face meetings and taking people at face value. It believes in using first names, good faith and eye contact.</p>
 
 	<p>Neutral Mediation encourages people to be friendly and empathetic, honest and direct, trustworthy, energetic, prepared, composed, positive and civilized. It believes in brainstorming, open-minds and the strengthening of communication through listening and storytelling. It believes in the power of emotional vulnerability to transform disputes.</p>
-
-	<p>It believes that a neutral’s words matter and should be used only necessarily, and that a neutral’s word matters and should be indefatigably trustworthy.</p>
-
-	<p><em>Neutral Mediation’s EIN is 87-2105551. <a href="/w9">Click here for a downloadable W-9</a>.</em></p>
 
 	<h4>More Information</h4>
 
@@ -52,11 +50,3 @@
 	</ul>
 
 </div>
-
-<style>
-	.content {
-		width: 100%;
-		max-width: var(--column-width);
-		margin: var(--column-margin-top) auto 0 auto;
-	}
-</style>
