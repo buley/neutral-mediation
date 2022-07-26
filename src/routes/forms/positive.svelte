8a5a3cd01657a4d65214de72a3938a0bacc8137c
Taylor Buley <buley@outlook.com>
2022-07-25 21:30:44 -0700

    .

---
+++ src/routes/forms/positive.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+	export const hydrate = dev;
+	export const router = browser;
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Positive Conflict Analysis Form</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<div class="content">
+	<h1>Positive Conflict Analysis</h1>
+	<p>
+		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeepa2YNEypg9GpGJgKKgs-2vmvRXsM9Rt3S_8-N2eEviq0Ng/viewform" border=0 style="width:100%;min-height:800px;">
+	</p>
+</div>
