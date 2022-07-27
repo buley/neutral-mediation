d9a18500af0272d96468e92e45f4ba400c1aac76
Taylor Buley <buley@outlook.com>
2022-07-26 21:44:15 -0700

    .

--- src/routes/forms/communication.svelte
+++ src/routes/forms/communication.svelte
@@ -6,12 +6,12 @@
 </script>
 
 <svelte:head>
-	<title>Self-Assessment Form</title>
+	<title>Communication Analysis Form</title>
 	<meta name="description" content="" />
 </svelte:head>
 
 <div class="content">
-	<h1>Self-Assessment</h1>
+	<h1>Communication Analysis</h1>
 	<p>
 		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmyejcc746eZ6N4WT_k1HnQjbM3aKC_SCO-eH_GXCN3pM0uQ/viewform" border=0 style="width:100%;min-height:800px;">
 	</p>
