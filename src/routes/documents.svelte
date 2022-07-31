fa0becbed1d30887404fce2b86fa6370983607d4
Taylor Buley <buley@outlook.com>
2022-07-31 11:49:59 -0700

    .

--- src/routes/documents.svelte
+++ src/routes/documents.svelte
@@ -33,7 +33,8 @@
 		<li><a href="/forms/communication">Communication Analysis</a></li>
 		<li><a href="/forms/conflict">Conflict Analysis</a></li>
 		<li><a href="/forms/goals">Goals Analysis</a></li>
-		<li><a href="/forms/conflict">Positive Conflict Analysis</a></li>
+		<li><a href="/forms/layered">Layered Conflict Analysis</a></li>
+		<li><a href="/forms/positive">Positive Conflict Analysis</a></li>
 		<li><a href="/forms/self-assessment">Self-Assessment</a></li>
 	</ul>
 	<h3>Questionnaires</h3>
---
+++ src/routes/forms/layered.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+	export const hydrate = dev;
+	export const router = browser;
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Layered Conflict Analysis Form</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<div class="content">
+	<h1>Layered Conflict Analysis</h1>
+	<p>
+		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRN1ZXoSwJWaq9Q677UDBhwpvNIehfVdrWp0G2-8gkIY9LCg/viewform" border=0 style="width:100%;min-height:800px;">
+	</p>
+</div>
