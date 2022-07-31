d321f61137548fe8be9117f4b33c697f3150b870
Taylor Buley <buley@outlook.com>
2022-07-31 14:06:03 -0700

    .

--- src/routes/forms/honesty.svelte
+++ src/routes/forms/honesty.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <svelte:head>
-	<title>Goal Analysis Form</title>
+	<title>Honesty Analysis Form</title>
 	<meta name="description" content="" />
 </svelte:head>
 
 <div class="content">
-	<h1>Goal Analysis</h1>
+	<h1>Honesty Analysis</h1>
 	<p>
-		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdeRm9vg250M0qh7HohEryMR3ChC1SFsM8HMYGCznNewrQ35w/viewform" border=0 style="width:100%;min-height:800px;">
+		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc0iKMi_JTV_SPz59HCZ_XbCLoJuu5SOs6Y29AOnmzVmmw_Dw/viewform" border=0 style="width:100%;min-height:800px;">
 	</p>
 </div>
