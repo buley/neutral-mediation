6e0cfef10f3ddab400b7a78023588f0ca7210a7a
Taylor Buley <buley@outlook.com>
2022-07-31 14:19:39 -0700

    .

--- src/routes/forms/root.svelte
+++ src/routes/forms/root.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <svelte:head>
-	<title>Root Emotional Analysis Form</title>
+	<title>Positive Conflict Analysis Form</title>
 	<meta name="description" content="" />
 </svelte:head>
 
 <div class="content">
-	<h1>Root Emotional Analysis</h1>
+	<h1>Positive Conflict Analysis</h1>
 	<p>
-		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScrvy6Q_V7OX4jpGjjzIZmjSqw6xjFsdtOTBHSfkmK7WxPNrg/viewform" border=0 style="width:100%;min-height:800px;">
+		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeepa2YNEypg9GpGJgKKgs-2vmvRXsM9Rt3S_8-N2eEviq0Ng/viewform" border=0 style="width:100%;min-height:800px;">
 	</p>
 </div>
