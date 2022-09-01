c52226739181223bf4ef604ec0148745f87208a9
Taylor Buley <buley@outlook.com>
2022-08-31 22:50:17 -0700

    .

--- src/routes/pages/mediation/how-to-involve-a-neutral.svelte
+++ src/routes/pages/mediation/how-to-involve-a-neutral.svelte
@@ -13,15 +13,15 @@
 <div class="content">
 	<h1>How To Involve a Neutral</h1>
 	
-	<p>Mediators aren’t licensed. Mediations not legally binding by default.&nbsp;Mediation acts in a dozen states.</p>
+	<p>Mediators aren’t licensed and mediations are not legally binding by default. But there are mediation acts in a dozen states that can be used by trained neutrals to achieve enforcible agreements.</p>
 	
-	<p>What does a mediator do?</p>
+	<h4>What does a mediator do?</h4>
 	
 	<p>Mediators:</p>
 	
 	<ul><li>Help deal with emotional clients<ul><li>15 minute therapy</li></ul></li><li>Give bad news</li><li>Allow lawyers to maintain warrior status</li><li>Listen and absorb clients anger</li><li>Empathize with the clients</li><li>Advocate for compromise</li></ul>
 	
-	<p>Lawyers are called counselors, but mediator’s are happy to do counselling. More range beyond legal issues. Non-lawyerly techniques.&nbsp;</p>
+	<p>Lawyers are called counselors, but mediators are happy to do counseling. More range beyond legal issues. Non-lawyerly techniques.&nbsp;</p>
 	
 	<p>Help professional with unrealistic or otherwise difficult clients:</p>
 	
@@ -31,19 +31,19 @@
 	
 	<ul><li>Mediator asks what goals are for the relationship between client and mediator?</li><li>Creative options</li><li>Idea person</li><li>Avoid reactive devaluation<ul><li>Normal</li><li>Excessive and endemic</li></ul></li><li>Mediator not just not enemy, but is ally<ul><li>Bond with client</li><li>Build trust</li></ul></li><li>Carry water for demands<ul><li>Can’t take side</li><li>Can carry message</li></ul></li><li>Gather information<ul><li><em>What’s happening on the other side?</em></li><li><em>How is the offer going to be received?</em></li></ul></li></ul>
 	
-	<p>Help parties find solutions.</p>
+	<p>Help parties find solutions</p>
 	
-	<p>Relentless only in pursuit of agreement&nbsp;</p>
+	<p>Relentless but only in pursuit of agreement</p>
 	
-	<p>Creates a Safe setting</p>
+	<p>Creates a safe communication setting</p>
 	
-	<p>Agreement scribe&nbsp;</p>
+	<p>Agreement scribe</p>
 	
-	<p>Participation, authority, preparation, briefs&nbsp;</p>
+	<p>Brings participation, authority and preparation</p>
 	
-	<p>Creative options</p>
+	<p>Facilitate creative options</p>
 	
-	<p>Transformative</p>
+	<p>Create transformative discussions</p>
 
 </div>
 
