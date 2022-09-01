9d135c88eeacb644380a6ed8f2aea7629475731f
Taylor Buley <buley@outlook.com>
2022-08-31 22:23:05 -0700

    .

--- src/routes/cancellation.svelte
+++ src/routes/cancellation.svelte
@@ -13,13 +13,13 @@
 <div class="content">
 	<h3>Cancellation Policy</h3>
 	<p>
-		Schedules are tight and abrupt cancellations can come at the opportunity cost of solving other customer's problems. A session or training can be rescheduled for a $175 fee any time up until seven calendar days before the scheduled engagement.
+		Schedules are tight and abrupt cancellations can come at the opportunity cost of solving other customers' problems. A session or training can be rescheduled for a $175 fee any time up until seven calendar days before the scheduled engagement.
 	</p>
 	<p>
-		A mediation session will not be considered fully-scheduled until all parties complete the required paperwork and pay their participation fees, upon which a Mediation Engagement Letter is sent out and signed by all parties. A partially-scheduled meditation can similarly be rescheduled for a fee, up until seven calendar days before the engagement date.
+		Neutral Mediation does not consider a mediation session to be  scheduled until all parties complete the required paperwork and pay their participation fees, after which the neutral will send out, upon request, a Mediation Engagement Letter. Clients can reschedule a partially-scheduled meditation for a fee up until seven calendar days before the engagement date.
 	</p>
 	<p>
-		Neutral Mediation reserves the right to withdraw from an engagement at any time, for any reason, and in that case will refund all payments. There are no refunds on canceled sessions and training except in the very narrow circumstances defined in the <a href="/refunds">refund policy</a>.
+		Neutral Mediation reserves the right to withdraw from an engagement at any time, for any reason, and, in that case, will refund all payments. There are no refunds on canceled sessions and training except in the very narrow circumstances defined in the <a href="/refunds">refund policy</a>.
 	</p>
 </div>
 
--- src/routes/consulting.svelte
+++ src/routes/consulting.svelte
@@ -16,29 +16,27 @@
 	
 	<h2>Our Practice</h2>
 	<p>
-		Neutral Mediation offers organizations flexibly-scheduled virtual alternative dispute resolution consulting services &mdash; such as public and private meeting facilitation, conflict audits and reporting, and dispute resolution systems design &mdash; tailored to meet specific business and nonprofit goals. <a href="/background">Read about our neutral here</a>. Optionally, consulting services can take place on-site at an organization or an agreed-upon location, pursuant to the <a href="/prices">travel policy</a>. <em>Consulting sessions cost $175 per hour per participant.</em> For individual-focused coaching sessions, see our <a href="/coaching">coaching page</a>. For neutral-led mediation sessions, see our <a href="/mediation">mediation page</a>. For neutral-led negotiation sessions, see our <a href="/negotiation">negotiation page</a>. For mediation and negotiation training, see our <a href="/training">training page</a>.
+		Neutral Mediation offers organizations flexible virtual options for   dispute resolution consulting services &mdash; such as public and private meeting facilitation, conflict audits and reporting, and dispute resolution systems design &mdash; tailored to meet specific business and nonprofit goals. <a href="/background">Read about our neutral here</a>. Optionally, consulting services can take place on-site at an organization or an agreed-upon location, pursuant to the <a href="/prices">travel policy</a>. <em>Consulting sessions cost $175 per hour per participant.</em> For individual-focused coaching sessions, see our <a href="/coaching">coaching page</a>. For neutral-led mediation sessions, see our <a href="/mediation">mediation page</a>. For neutral-led negotiation sessions, see our <a href="/negotiation">negotiation page</a>. For mediation and negotiation training, see our <a href="/training">training page</a>.
 	</p>
 	<h2>Our Value</h2>
-	<p>
-		A consultant spares emotional trauma, time, and expense associated with organizational systems that generate chronic conflict.
+	<p>A consultant spares emotional trauma, time, and expense associated with organizational systems that generate chronic conflict.
 	</p>
-	<p>
-		A neutral creates a safe environment in which participants can develop rapport and explore the emotional vulnerability necessary to move beyond conflict. A neutral teaches and uses tools like active listening, empathy, and creativity to inspire team building, challenge limiting and false beliefs, and generate ideas that move people forward.
+	<p>A neutral creates a safe environment in which participants can develop rapport and explore the emotional vulnerability necessary to move beyond conflict. A neutral teaches and uses tools like active listening, empathy, and creativity to inspire team building, challenge limiting and false beliefs, and generate ideas that move people forward.
 	</p>
 
 	<h2>Our Services</h2>
 
 	<h3>Conflict Consulting</h3>
 
-	<p>Neutral-led conflict virtual training session for one, four, or eight hours.  Conflict consulting engagements are typically focused on addressing organizational systems that generate chronic conflict. <em>Conflict consulting costs $175 per hour per participant.</em> <a href="https://neutral.setmore.com/">Click here to book a conflict consultation</a></p>
+	<p>A neutral-led conflict virtual training session for one, four, or eight hours. Conflict consulting engagements typically focus on addressing organizational systems that generate chronic conflict. <em>Conflict consulting costs $175 per hour per participant.</em> <a href="https://neutral.setmore.com/">Click here to book a conflict consultation</a></p>
 
 	<h3>Mediation Consulting</h3>
 
-	<p>Neutral-led virtual mediation consulting session for one, four, or eight hours, optionally on-site at an organization. Mediation engagements are typically concerning community facilitation, collaborative discussion, or designing internal mediation processes for risk management.  <em>Mediation consulting costs $175 per hour per participant.</em> <a href="https://booking.setmore.com/scheduleappointment/825a522f-34f5-4a37-98e1-668e1788cb07/services/sbcc6e0750eb25cc175571dd6570a5adc80e1aa1e">Click here to book a consulting session</a>. For neutral-led mediation sessions, see our <a href="/mediation">mediation page</a>.</p>
+	<p>A neutral-led virtual mediation consulting session for one, four, or eight hours, optionally on-site at an organization. Mediation engagements  typically concern community facilitation, collaborative discussion, or designing internal mediation processes for risk management.  <em>Mediation consulting costs $175 per hour per participant.</em> <a href="https://booking.setmore.com/scheduleappointment/825a522f-34f5-4a37-98e1-668e1788cb07/services/sbcc6e0750eb25cc175571dd6570a5adc80e1aa1e">Click here to book a consulting session</a>. For neutral-led mediation sessions, see our <a href="/mediation">mediation page</a>.</p>
 
 	<h3>Negotiation Consulting</h3>
 	
-	<p>Neutral-led virtual negotiation consulting session for one, four, or eight hours, optionally on-site at an organization. Consulting engagements typically concern analyzing or planning for a specific negotiation or similar set of negotiations. <em>Negotiation consulting costs $175 per hour per participant.</em> <a href="https://booking.setmore.com/scheduleappointment/825a522f-34f5-4a37-98e1-668e1788cb07/services/5eb4c115-1efc-478d-b908-69875ef1303d">Click here to book a negotiation consulting session</a>.</p>
+	<p>A neutral-led virtual negotiation consulting session for one, four, or eight hours, optionally on-site at an organization. Consulting engagements typically concern analyzing or planning for a specific negotiation or similar set of negotiations. <em>Negotiation consulting costs $175 per hour per participant.</em> <a href="https://booking.setmore.com/scheduleappointment/825a522f-34f5-4a37-98e1-668e1788cb07/services/5eb4c115-1efc-478d-b908-69875ef1303d">Click here to book a negotiation consulting session</a>.</p>
 
 </div>
 
