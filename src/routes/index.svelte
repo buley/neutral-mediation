a288f8bfe9f3a6aa247eac569d4a2e4fdf05dd00
Taylor Buley <buley@outlook.com>
2022-09-09 00:34:52 -0700

    .

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <svelte:head>
-	<title>Neutral Mediation</title>
+	<title>Neutral Mediation - Northern California Mediation</title>
 	<meta name="description" content="Neutral Mediation minimizes the emotional trauma, time and expense associated with complex conflict through the practice of neutral-led mediation and negotiation, in addition to training, consulting nad conflict coaching." />
 	<MetaTags title="Neutral Mediation" description="Neutral Mediation minimizes the emotional trauma, time and expense associated with complex conflict through the practice of neutral-led mediation and negotiation, in addition to training, consulting nad conflict coaching." />
 </svelte:head>
