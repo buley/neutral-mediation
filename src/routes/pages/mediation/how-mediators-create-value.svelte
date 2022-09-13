935a7ef7cbd31592406d6be6e590c97d213ad08b
Taylor Buley <buley@outlook.com>
2022-09-12 20:55:33 -0700

    .

---
+++ src/routes/pages/mediation/how-mediators-create-value.svelte
@@ -0,0 +1,42 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+	export const hydrate = dev;
+	export const router = browser;
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>How Mediators Create Value</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<div class="content">
+	<h1>How Mediators Create Value</h1>
+	
+	<h4>Safe, Confidential Environment</h4>
+
+	<h4>Fast Resolutions</h4>
+
+	<h4>Cost Minimization</h4>
+	
+	<ul><li>Health costs</li><li>Relationship costs<ul><li>Collateral damage</li></ul></li><li>Opportunity cost</li><li>Reputation costs</li><li>Risk factors</li><li>Certainty of amounts</li><li>Finality of dispute</li><li>Time/value of $</li></ul>
+	
+	<h4>Positivity & Creativity</h4>
+
+	<h4>Active Listening</h4>
+
+	<h4>Directed Questioning</h4>
+
+	<h4>Offer Facilitation</h4>
+	
+	<h4>Mediator’s Proposal</h4>
+
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
---
+++ src/routes/pages/mediation/limits-of-mediation.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+	export const hydrate = dev;
+	export const router = browser;
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Limits of Mediation</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<div class="content">
+	<h1>Limits of Mediation</h1>
+	<p>We're sorry for the inconvenience because this content is not yet available.</p>
+
+<!--
+
+-->
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
