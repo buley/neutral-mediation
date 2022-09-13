0511cf3ad437d9f3e45b8f3b15e10f64724e7fa6
Taylor Buley <buley@outlook.com>
2022-09-12 21:42:45 -0700

    .

--- src/routes/pages/quotes/emotion.svelte
+++ src/routes/pages/quotes/emotion.svelte
@@ -9,6 +9,35 @@
 	<title>Useful Emotion Quotes</title>
 	<meta name="description" content=""     />
 	<!--
+<JsonLd
+  schema={{
+    '@type': 'Article',
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': 'https://example.com/article'
+    },
+    headline: 'Article headline',
+    image: [
+      'https://example.com/photos/1x1/photo.jpg',
+      'https://example.com/photos/4x3/photo.jpg',
+      'https://example.com/photos/16x9/photo.jpg'
+    ],
+    datePublished: '2015-02-05T08:00:00+08:00',
+    dateModified: '2015-02-05T09:20:00+08:00',
+    author: {
+      '@type': 'Person',
+      name: 'John Doe'
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: 'Google',
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://example.com/logo.jpg'
+      }
+    }
+  }}
+/>
 	<MetaTags
 	  title="Using More of Config"
 		titleTemplate = '%s | Svelte Meta Tags'
